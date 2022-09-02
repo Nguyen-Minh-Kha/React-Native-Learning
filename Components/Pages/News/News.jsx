1593da29a3360c87eccc2c41ac99da8d364b50c9
NGUYEN Minh Kha <pro.nguyenminhkha@gmail.com>
2022-09-02 15:01:15 +0200

    add search function

--- Components/Pages/News/News.jsx
+++ Components/Pages/News/News.jsx
@@ -10,16 +10,25 @@ import {
 import { useEffect, useState } from "react";
 import axios from "axios";
 import * as WebBrowser from "expo-web-browser";
+import Button from "../UI/Button/Button";
+import InputWithError from "../UI/InputWithError/InputWithError";
 
 const News = () => {
   const [listNews, setListNews] = useState([]);
   const size = useWindowDimensions();
 
+  const [input, setInput] = useState();
+
+  const handleInput = (text) => {
+    setInput(text);
+  };
+
+  let request =
+    "https://newsapi.org/v2/top-headlines?country=fr&category=business&apiKey=d81a12d645404484a12ea88b7ff4219a";
+
   useEffect(() => {
     axios
-      .get(
-        "https://newsapi.org/v2/top-headlines?country=fr&category=business&apiKey=d81a12d645404484a12ea88b7ff4219a"
-      )
+      .get(request)
       .then((reponse) => {
         console.log(reponse);
         setListNews(reponse.data.articles);
@@ -29,8 +38,39 @@ const News = () => {
       });
   }, []);
 
+  const search = (text) => {
+    request =
+      "https://newsapi.org/v2/everything?q=" +
+      text +
+      "&apiKey=d81a12d645404484a12ea88b7ff4219a";
+    console.log(request);
+    axios
+      .get(request)
+      .then((reponse) => {
+        console.log(reponse);
+        setListNews(reponse.data.articles);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <ScrollView style={styles.container}>
+      <InputWithError
+        holder={"Entrez votre recherche ici"}
+        valeur={input}
+        action={handleInput}
+        type="default"
+      />
+      <Button
+        action={() => {
+          search(input);
+        }}
+      >
+        <Text style={{ color: "white" }}>Rechercher</Text>
+      </Button>
+
       {listNews.map((article, index) => {
         return (
           <TouchableOpacity
